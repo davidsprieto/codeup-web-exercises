552964effe4fab03bb093c72cc92a7d6b9aa8166
David Prieto <david@Davids-MBP.hsd1.il.comcast.net>
2022-03-11 17:38:58 -0600

    Added dataTypes-operators-variables & operators files. Completed primitive types exercises.

---
+++ js/primitive-types.js
@@ -0,0 +1,7 @@
+console.log("99.9 is a number.");
+console.log("'false' is a string.");
+console.log("false is a boolean.");
+console.log("'0' is a string.");
+console.log("0 is a number.");
+console.log("true is a boolean.");
+console.log("'true' is a string.");
