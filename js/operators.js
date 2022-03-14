93daff1fa0c9d71d105a49d2e27d30b030a12243
David Prieto <david@Davids-MBP.hsd1.il.comcast.net>
2022-03-14 13:35:36 -0500

    Completed Operators Exercises Notes

--- js/operators.js
+++ js/operators.js
@@ -1,35 +1,39 @@
-'1' + 2
+// '1' + 2
+    // 12
+// typeof '1' + 2
+    // string2
+// 6 % 4
+    // 2
+// '3 + 4 is ' + 3 + 4
+    // 3 + 4 is 34
+// '3 + 4 is ' + (3 + 4)
+    // 3 + 4 is 7
+// 0 < 0
+    // false
+// 'false' == false
+    // false
+// true == 'true'
+    // true
+// 5 >= -5
+    // true
+// !false || false
+    // true
+// true || "42"
+    // true
+// !true && !false
+    // false
+// 6 % 5
+    // 1
+// 5 < 4 && 1 === 1
+    // false
+// typeof 'codeup' === 'string'
+    // true
+// 'codeup' === 'codeup' && 'codeup' === 'Codeup'
+    // false
+// 4 >= 0 && 1 !== '1'
+    // false
+// 6 % 3 === 0
+    // true
+// 5 % 2 !== 0
+    // true
 
-typeof '1' + 2
-
-6 % 4
-
-'3 + 4 is ' + 3 + 4
-
-0 < 0
-
-'false' == false
-
-true == 'true'
-
-5 >= -5
-
-!false || false
-
-true || "42"
-
-!true && !false
-
-6 % 5
-
-5 < 4 && 1 === 1
-
-typeof 'codeup' === 'string'
-
-'codeup' === 'codeup' && 'codeup' === 'Codeup'
-
-4 >= 0 && 1 !== '1'
-
-6 % 3 === 0
-
-5 % 2 !== 0
