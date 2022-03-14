a1468fc8a4829839fd96d378c2bcff3ead1ae8ee
David Prieto <david@Davids-MBP.hsd1.il.comcast.net>
2022-03-14 12:58:16 -0500

    Completed primitive types notes.

--- js/primitive-types.js
+++ js/primitive-types.js
@@ -6,3 +6,18 @@ console.log("'0' is a string.");
 console.log("0 is a number.");
 console.log("true is a boolean.");
 console.log("'true' is a string.");
+
+// What data type would best represent:
+
+// A term or phrase typed into a search box?
+    // String
+// If a user is logged in?
+    // Boolean
+// A discount amount to apply to a user's shopping cart?
+    // Number
+// Whether or not a coupon code is valid?
+    // Boolean
+// An email address typed into a registration form?
+    // String
+// The price of a product?
+    // Number
