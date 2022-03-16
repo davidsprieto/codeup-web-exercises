3f908448fb38bfb7f8356a06aadf57933328ad82
David Prieto <david@Davids-MBP.hsd1.il.comcast.net>
2022-03-16 14:29:33 -0500

    Created in-class-js-notes-and-exercises.js for in class notes & example problems.

---
+++ js/in-class-js-notes-and-exercises.js
@@ -0,0 +1,11 @@
+var name = "Laura"
+
+if (name === "Laura"){
+    console.log("Hey, she's my instructor.");
+} else if (name === "Kenneth"){
+    console.log("Oh, he's my instructor too!");
+} else {
+    console.log("Um, I don't know them.")
+}
+
+
