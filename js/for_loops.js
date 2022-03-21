07266a4d702c736bd370fb687fe4bfb926349204
David Prieto <david@Davids-MBP.hsd1.il.comcast.net>
2022-03-21 13:40:51 -0500

    Created for_loops.js for JS For Loops Exercises.

---
+++ js/for_loops.js
@@ -0,0 +1,32 @@
+// JS For Loops Exercises:
+
+// Exercise #2:
+function showMultiplicationTable(numberToMultiply) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(numberToMultiply * i);
+    }
+}
+showMultiplicationTable(7);
+
+
+// Exercise #3:
+for (var a = 1; a <= 10; a++) {
+    var randomNumber = Math.floor(Math.random() * 181) + 20;
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even.");
+    } else if (randomNumber % 2 === 1) {
+        console.log(randomNumber + " is odd.");
+    }
+}
+
+
+// Exercise #4:
+for (var b = 1; b <= 9; b++) {
+    console.log(b.toString().repeat(b));
+}
+
+
+// Exercise #5:
+for (var c = 100; c >= 5; c = c - 5) {
+    console.log(c);
+}
