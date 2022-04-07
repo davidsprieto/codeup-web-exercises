8f46adb9262770d1b869cfdf7c2e770dcf362ce5
David Prieto <david@Davids-MBP.hsd1.il.comcast.net>
2022-04-07 11:02:35 -0500

    Completed CSS Flexbox In Practice Exercise.

--- js/groovy-morning-warmup-exercies.js
+++ js/groovy-morning-warmup-exercies.js
@@ -150,7 +150,7 @@ console.log(sortArr(arraySort));
 // solution found on stackoverflow.com:
 function getFrequency(string) {
     var freq = {};
-    for (var i=0; i < string.length; i++) {
+    for (var i = 0; i < string.length; i++) {
         var character = string.charAt(i);
         if (freq[character]) {
             freq[character]++;
