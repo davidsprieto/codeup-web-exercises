b6e27db92f78a9ad6d820454e3df28b6bfb27bce
David Prieto <david@Davids-Air.hsd1.il.comcast.net>
2022-04-27 12:17:18 -0500

    Updated to correct date.

--- js/groovy-morning-warmup-exercies.js
+++ js/groovy-morning-warmup-exercies.js
@@ -387,7 +387,7 @@ function negativeFrequency(array) {
 console.log(negativeFrequency(arrayNums));
 
 
-// 04/26/22 - Write a function that takes two date instances as argument:
+// 04/27/22 - Write a function that takes two date instances as argument:
 // It should return the number of days that lies between those dates
 // example input: new Date('2020-06-11'), new Date('2020-06-01')
 // expected output: 10
@@ -402,7 +402,7 @@ function daysBetweenDates(a, b) {
 console.log(daysBetweenDates(date1, date2));
 
 
-// 04/26/22 - // Write a function that takes an array (a) and a number (b) as arguments:
+// 04/27/22 - // Write a function that takes an array (a) and a number (b) as arguments:
 // Sum up all array elements with a value greater than b
 // Return the sum
 // example input: [1, 2, 3, 4, 5, 6, 7], 2
