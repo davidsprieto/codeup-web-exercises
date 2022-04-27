ce0a37baf52d904854f9b6957bf3bbed29feb90e
David Prieto <david.s.prieto1@gmail.com>
2022-04-27 18:41:44 -0500

    refactored konami code exercise.

--- js/jquery-konami-code.js
+++ js/jquery-konami-code.js
@@ -8,24 +8,12 @@
 function konamiCheatCode() {
     var konamiKeys = '3838404037393739666513';
     var userTyping = '';
-    userTyping = userTyping + $(document).keyup(function(event){
+    $(document).keyup(function(event) {
         console.log(event.keyCode);
+        userTyping = userTyping + ('' + event.keyCode);
+        if (userTyping === konamiKeys) {
+            return alert('You have unlocked 30 lives!');
+        }
     });
-    if (konamiKeys === userTyping) {
-        alert('You have unlocked 30 lives!');
-    }
 }
 konamiCheatCode();
-
-// function konamiCheatCode() {
-//     var konamiKeys = '3838404037393739666513';
-//     var userTyping = '';
-//     $(document).keyup(function (event) {
-//         console.log(event.keyCode);
-//         userTyping = userTyping + ("" + event.keyCode);
-//         if (userTyping === konamiKeys) {
-//            return alert('You have unlocked 30 lives!');
-//         }
-//     })
-// }
-// konamiCheatCode();
