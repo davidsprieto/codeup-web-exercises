f4cfb60685f299e82b5de08c1cb15e20431ca24d
David Prieto <david.s.prieto1@gmail.com>
2022-04-27 15:34:35 -0500

    Completed JQuery Mouse Events Exercises.

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,10 +1,14 @@
 "use strict"
 
 // JQuery Introduction Exercise:
+//
 // $(document).ready(function() {
 //     alert('The DOM has finished loading!');
 // });
 
+
+// JQuery Selectors Exercise:
+//
 // Id Selectors Exercises:
 // var lackland = $('#lackland').html();
 // alert(lackland);
@@ -23,4 +27,25 @@
 // alert(heading);
 
 // Multiple Selectors Exercise:
-$('h1, p, li').css('background-color', 'yellow');
+// $('h1, p, li').css('background-color', 'yellow');
+
+
+// Mouse Events Exercises:
+function changeHeadingColor() {
+    $(this).css('background-color', 'blue').css('color', 'white');
+}
+$('#heading').click(changeHeadingColor);
+
+function changePFontSize() {
+    $(this).css('font-size', '18px');
+}
+$('p').dblclick(changePFontSize);
+
+function changeLiTextColor() {
+    $(this).css('color','red');
+}
+
+function changeLiToDefaultColor() {
+    $(this).css('color','black');
+}
+$('li').hover(changeLiTextColor, changeLiToDefaultColor);
