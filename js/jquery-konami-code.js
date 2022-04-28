1a549942abe048008293dcf9bc506e91e7f22a7d
David Prieto <david.s.prieto1@gmail.com>
2022-04-28 14:41:06 -0500

    added css code to change page layout after konami code function is ran.

--- js/jquery-konami-code.js
+++ js/jquery-konami-code.js
@@ -12,7 +12,16 @@ function konamiCheatCode() {
         console.log(event.keyCode);
         userTyping = userTyping + ('' + event.keyCode);
         if (userTyping === konamiKeys) {
-            return alert('You have unlocked 30 lives!');
+            alert('You have unlocked 30 lives!');
+            $('body').css({
+                'background-image': 'url("https://cdn.vox-cdn.com/thumbor/aoQFyyKR4mHMRYArszFQr1r5VcY=/0x0:1024x768/920x613/filters:focal(431x303:593x465):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66380852/ERtzjdMUwAEsRXl.0.png")',
+                'background-repeat': 'no-repeat',
+                'background-size': 'cover',
+            });
+            $('#konami').css({
+                'text-align': 'center',
+                'color': 'white'
+            });
         }
     });
 }
