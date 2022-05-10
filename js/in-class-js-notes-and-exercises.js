0e3e7727f4be8ba129206c7393f768d68d66d43d
David Prieto <david.s.prieto1@gmail.com>
2022-05-10 15:01:50 -0500

    modified code for better readability.

--- js/in-class-js-notes-and-exercises.js
+++ js/in-class-js-notes-and-exercises.js
@@ -86,11 +86,15 @@ console.log(positivesNumbersSum);
 // Example Input: [12, 46, 32, 64];
 // Example Output: {mean: 38.5, median: 32};
 const input = [12, 46, 32, 64];
+
 input.sort((a, b) => a - b);
+
 input.reduce((accumulator, currentValue, index, array) => {
-    accumulator.mean += currentValue /  array.length;
+    accumulator.mean = accumulator.mean + currentValue /  array.length;
+
     if(Math.abs(index + 1  - array.length / 2) < 1) {
-            accumulator.median = currentValue
+        accumulator.median = currentValue
     }
+
     return accumulator;
 }, { mean: 0, median: 0 });
