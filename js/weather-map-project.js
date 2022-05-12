914fe211b1e7bd337099577218f9c1949266892d
David Prieto <david.s.prieto1@gmail.com>
2022-05-11 21:16:47 -0500

    Modified JS Code.

--- js/weather-map-project.js
+++ js/weather-map-project.js
@@ -1,14 +1,16 @@
 "use strict";
 
-mapboxgl.accessToken = mapBoxKey;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-87.67731, 41.91907], // starting position [lng, lat]
-    zoom: 10.5 // starting zoom
-});
+// mapboxgl.accessToken = mapBoxKey;
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v11', // style URL
+//     center: [-87.67731, 41.91907], // starting position [lng, lat]
+//     zoom: 10 // starting zoom
+// });
 
 $.get("https://api.openweathermap.org/data/2.5/weather", {
     APPID: openWeatherMapApiKey,
     q: "Chicago, US",
+}).done(function(data) {
+    console.log(data)
 });
