fdab70aeb51a2ad5baf4d12ccab71e36ec36ad6b
David Prieto <david.s.prieto1@gmail.com>
2022-05-17 18:58:46 -0500

    Updated styling. Added comments and cleaned up code.

--- js/weather_map.js
+++ js/weather_map.js
@@ -4,7 +4,7 @@
 var latitude = 42.3703;
 var longitude = -87.902;
 
-// Function that makes the initial get request to the open weather api to obtain the 5-day forecast data --------//
+// Function that makes the get request to the open weather api to obtain the 5-day forecast data --------//
 function retrieveData(location) {
     $.get("https://api.openweathermap.org/data/2.5/onecall", {
         APPID: openWeatherMapApiKey,
@@ -26,14 +26,13 @@ function retrieveData(location) {
 retrieveData("Gurnee, US");
 
 // Display weather functions -----------//
-// Should refactor code at a later time to iterate through the data object in order to only have one function --------//
 function displayWeather0(data) {
-    let iconcode = data.daily[0].weather[0].icon;
-    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    let iconCode = data.daily[0].weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     const dayJsObject = dayjs();
     $('#date0').html('Date: ' + dayJsObject.format("M/D/YYYY"));
     $('#high-low0').html("High: " + data.daily[0].temp.max.toString() + " / Low: " + data.daily[0].temp.min.toString());
-    $('#image0').attr('src', iconurl);
+    $('#image0').attr('src', iconUrl);
     $('#description0').html("Description: " + data.daily[0].weather[0].description);
     $('#humidity0').html("Humidity: " + data.daily[0].humidity);
     $('#wind0').html("Wind Speed: " + data.daily[0].wind_speed)
@@ -41,12 +40,12 @@ function displayWeather0(data) {
 }
 
 function displayWeather1(data) {
-    let iconcode = data.daily[1].weather[0].icon;
-    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    let iconCode = data.daily[1].weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     const dayJsObject = dayjs();
     $('#date1').html('Date: ' + dayJsObject.add(1, 'day').format("M/D/YYYY"));
     $('#high-low1').html("High: " + data.daily[1].temp.max.toString() + " / Low: " + data.daily[1].temp.min.toString());
-    $('#image1').attr('src', iconurl);
+    $('#image1').attr('src', iconUrl);
     $('#description1').html("Description: " + data.daily[1].weather[0].description);
     $('#humidity1').html("Humidity: " + data.daily[1].humidity);
     $('#wind1').html("Wind Speed: " + data.daily[1].wind_speed)
@@ -54,12 +53,12 @@ function displayWeather1(data) {
 }
 
 function displayWeather2(data) {
-    let iconcode = data.daily[2].weather[0].icon;
-    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    let iconCode = data.daily[2].weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     const dayJsObject = dayjs();
     $('#date2').html('Date: ' + dayJsObject.add(2, 'day').format("M/D/YYYY"));
     $('#high-low2').html("High: " + data.daily[2].temp.max.toString() + " / Low: " + data.daily[2].temp.min.toString());
-    $('#image2').attr('src', iconurl);
+    $('#image2').attr('src', iconUrl);
     $('#description2').html("Description: " + data.daily[2].weather[0].description);
     $('#humidity2').html("Humidity: " + data.daily[2].humidity);
     $('#wind2').html("Wind Speed: " + data.daily[2].wind_speed)
@@ -67,12 +66,12 @@ function displayWeather2(data) {
 }
 
 function displayWeather3(data) {
-    let iconcode = data.daily[3].weather[0].icon;
-    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    let iconCode = data.daily[3].weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     const dayJsObject = dayjs();
     $('#date3').html('Date: ' + dayJsObject.add(3, 'day').format("M/D/YYYY"));
     $('#high-low3').html("High: " + data.daily[3].temp.max.toString() + " / Low: " + data.daily[3].temp.min.toString());
-    $('#image3').attr('src', iconurl);
+    $('#image3').attr('src', iconUrl);
     $('#description3').html("Description: " + data.daily[3].weather[0].description);
     $('#humidity3').html("Humidity: " + data.daily[3].humidity);
     $('#wind3').html("Wind Speed: " + data.daily[3].wind_speed)
@@ -80,12 +79,12 @@ function displayWeather3(data) {
 }
 
 function displayWeather4(data) {
-    let iconcode = data.daily[4].weather[0].icon;
-    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    let iconCode = data.daily[4].weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     const dayJsObject = dayjs();
     $('#date4').html('Date: ' + dayJsObject.add(4, 'day').format("M/D/YYYY"));
     $('#high-low4').html("High: " + data.daily[4].temp.max.toString() + " / Low: " + data.daily[4].temp.min.toString());
-    $('#image4').attr('src', iconurl);
+    $('#image4').attr('src', iconUrl);
     $('#description4').html("Description: " + data.daily[4].weather[0].description);
     $('#humidity4').html("Humidity: " + data.daily[4].humidity);
     $('#wind4').html("Wind Speed: " + data.daily[4].wind_speed)
